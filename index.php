84c9196e5c891fee40bd12bcf72228380cd9d866
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-03-10 03:01:52 +0000

    index.php

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
 	<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Area to Perimeter, in PHP</title>
+    <title>Area and perimeter of a rectangle, in PHP</title>
 	</head>
 	<body>
-    <h1><?php echo 'Hello, World! in PHP'; ?></h1>
-		<?php echo '<p>This is my first web page using PHP.</p>'; ?>
+    <h1><?php echo 'Area and perimeter of a rectangle, in PHP'; ?></h1>
+    <?php echo "The dimensions of the rectangle are 15cm and 7cm."; ?>
+    <br>
+    <?php 
+    $length = 15; 
+    $width = 7;
+    echo "The area of the rectangle is $length * $width= " . ($length * $width) . "<br />";  
+    ?>
+    <?php
+    $length = 15;
+    $width = 7;
+    echo "The perimeter of the rectangle is " . ($length + $length + $width + $width) . "<br />";
+    ?>
   </body>
 </html>
-
-function doAreaClicked () {
-  // this function calculates the area of the rectangle
-<?php document.getElementById("multiply-math").innerHTML = "The area of the rectangle is " + (7 * 15) ?>
-}
-
-function doPerimeterClicked () {
-  // this function calculates the perimeter of the rectangle
-document.getElementById("add-math").innerHTML = "The perimeter of the rectangle is " + (7 + 7 + 15 + 15)
-}
