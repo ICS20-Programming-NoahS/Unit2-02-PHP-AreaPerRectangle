350063947a2ec9b25ca8931ccf29206b34dd07e9
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-03-10 02:00:11 +0000

    added README.md

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, World! in PHP</title>
+	</head>
+	<body>
+    <h1><?php echo 'Hello, World! in PHP'; ?></h1>
+		<?php echo '<p>This is my first web page using PHP.</p>'; ?>
+  </body>
+</html>
+
+function doAreaClicked () {
+  // this function calculates the area of the rectangle
+<?php document.getElementById("multiply-math").innerHTML = "The area of the rectangle is " + (7 * 15) ?>
+}
+
+function doPerimeterClicked () {
+  // this function calculates the perimeter of the rectangle
+document.getElementById("add-math").innerHTML = "The perimeter of the rectangle is " + (7 + 7 + 15 + 15)
+}
